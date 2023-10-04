1a43ae060ec6869803f1ce7da2d9b9796251f21f
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-04 18:24:04 +0300

    finished

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers
+
+apt-get -y install nginx
+if ! dpkg -l | grep -q nginx; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# create the fake html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# give ownership of the /data server to ubuntu user
+sudo chown -R ubuntu:ubuntu /data/
+
+# change the config to server hbnb_static
+nginx_config="/etc/nginx/sites-available/default"
+nginx_alias="location /hbnb_static/ { alias /data/web_static/current/; }"
+if ! grep -q "$nginx_alias" "$nginx_config"; then
+    sudo sed -i "/server_name _;/a $nginx_alias" "$nginx_config"
+fi
+
+# Restart Nginx
+sudo service nginx restart
