cb5c49698af2f429f0959c104f499aaea27d7acd
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-04 21:53:41 +0300

    finished

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,16 @@ from time import strftime
 from datetime import datetime
 import os
 
+
 def do_pack():
-	# creating a tar file
-	try:
-		now = datetime.now()
-		local("mkdir -p versions/")
-		time_format = now.strftime("%Y%m%d%H%M%S")
-		archive_name = "web_static_" + time_format + ".tgz"
-		archive_path = os.path.join("versions", archive_name)
-		local("tar -cvzf {} web_static".format(archive_path))
-		return archive_path
-	except Exception as e:
-		return None
+    # creating a tar file
+    try:
+        now = datetime.now()
+        local("mkdir -p versions/")
+        time_format = now.strftime("%Y%m%d%H%M%S")
+        archive_name = "web_static_" + time_format + ".tgz"
+        archive_path = os.path.join("versions", archive_name)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception as e:
+        return None
