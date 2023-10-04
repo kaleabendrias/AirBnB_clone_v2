e0f910990b4419a2a9f7454294583dbfeb33c808
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-04 14:27:59 -0400

    finished;

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+# import fabric
+from fabric.api import local
+from time import strftime
+from datetime import datetime
+import os
+
+def do_pack():
+	# creating a tar file
+	try:
+		now = datetime.now()
+		local("mkdir -p versions/")
+		time_format = now.strftime("%Y%m%d%H%M%S")
+		archive_name = "web_static_" + time_format + ".tgz"
+		archive_path = os.path.join("versions", archive_name)
+		local("tar -cvzf {} web_static".format(archive_path))
+		return archive_path
+	except Exception as e:
+		return None
