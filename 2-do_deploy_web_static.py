8fc6a8313f2f2ef7c8e93102036b56de819e686e
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-05 11:13:29 +0300

    finished

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,10 +21,8 @@ def do_deploy(archive_path):
         # uncompress the archive
         parts = archive_path.split('_')
         time_stamp = parts[2].split(".")[0]
-        run(f"sudo rm -rf /data/web_"
-            f"static/releases/web_static_{time_stamp}")
-        run("sudo mkdir -p /data/web_static/releases/\
-            web_static_{time_stamp}")
+        run(f"sudo rm -rf /data/web_static/releases/web_static_{time_stamp}")
+        run("sudo mkdir -p /data/web_static/releases/web_static_{time_stamp}")
         run(f"sudo tar -xvf /tmp/web_static_{time_stamp}.tgz -C /data/\
             web_static/releases/web_static_{time_stamp}")
 
