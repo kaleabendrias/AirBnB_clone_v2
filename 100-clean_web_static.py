f16072aa4d8d9e958b6e88bd548046fda9afae62
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-05 18:30:39 +0300

    finished

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,36 +8,21 @@ env.hosts = ['35.175.126.161', '54.164.52.24']
 
 def do_clean(number=0):
     """fabric method"""
-    try:
-        number = int(number)
-    except ValueError:
-        number = 0
-
-    if number < 0:
-        number = 0
-
-    # Get a list of all archives in the versions folder
-    archives = sorted(run('ls -1 /data/web_static/releases').split('\n'))
-
-    # Keep only the most recent `number` archives
-    if number >= len(archives):
+    if int(number) < 0:
         return
 
-    archives_to_delete = archives[:-number]
+    archives = sorted(run("ls -1t versions").splitlines())
 
-    # Delete archives in versions folder
-    for archive in archives_to_delete:
-        run('rm -f /data/web_static/releases/{}'.format(archive))
+    # Remove archives to keep
+    keep = archives[-int(number):]
+    archives = archives[:-int(number)]
 
-    # Get a list of all archives in the local versions folder
-    local_archives = local('ls -1 versions', capture=True).split('\n')
-
-    # Keep only the most recent `number` local archives
-    if number >= len(local_archives):
+    if len(archives) == 0:
         return
 
-    local_archives_to_delete = local_archives[:-number]
+    for archive in archives:
+        run("rm versions/{}".format(archive))
 
-    # Delete local archives
-    for archive in local_archives_to_delete:
-        local('rm -f versions/{}'.format(archive))
+    # Delete on web servers
+    with cd("/data/web_static/releases"):
+        run("ls -1t . | tail -n +{} | xargs rm -rf".format(len(keep) + 1))
