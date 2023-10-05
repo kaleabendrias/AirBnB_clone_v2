09cd48a70064de5e31c4cec0279a6d005d828537
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-05 15:11:23 +0300

    finished

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,6 +10,7 @@ env.hosts = ['35.175.126.161', '54.164.52.24']
 def do_deploy(archive_path):
     """deploys the archives to server"""
     try:
+        print("Starting do_deploy function...")
         if not (path.exists(archive_path)):
             return False
 
@@ -23,12 +24,17 @@ def do_deploy(archive_path):
 web_static_{}".format(time_stamp))
         run("sudo mkdir -p /data/web_static/releases/\
 web_static_{}".format(time_stamp))
-        run("sudo tar -xvf /tmp/web_static_{}.tgz -C /data/\
+        run("sudo tar -xzf /tmp/web_static_{}.tgz -C /data/\
 web_static/releases/web_static_{}".format(time_stamp, time_stamp))
 
         # delete the archive from web_server
         run("sudo rm -rf /tmp/web_static_{}.tgz".format(time_stamp))
 
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(time_stamp, time_stamp))
+        
+        #delete the file
+        run("rm -rf /data/web_static/releases/web_static_20170315003959/web_static")
         # delete the symbolic link
         run("sudo rm -rf /data/web_static/current")
 
