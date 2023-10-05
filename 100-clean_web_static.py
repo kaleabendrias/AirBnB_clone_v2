5d604f7014a3d1f2c10a7053a4180f72d8686286
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-05 17:51:18 +0300

    finished

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
 """a Fabric script"""
 from fabric.api import *
-import os
-
+from datetime import datetime
 
 env.hosts = ['35.175.126.161', '54.164.52.24']
 
 
 def do_clean(number=0):
-    """ deletes out-of-date archives"""
-    number = 1 if int(number) == 0 else int(number + 1)
+    """fabric method"""
+    try:
+        number = int(number)
+    except ValueError:
+        number = 0
 
-    # delete old archives in cersions folder
-    local("cd versions && ls -t | tail -n +\
-{} | xargs -I {{}} rm -f {{}}".format(number))
+    if number < 0:
+        number = 0
 
-    # delte old archives in /data/web_static_releases
-    run("cd /data/web_static/releases && ls -t | tail -n +\
-{} | xargs -I {{}} rm -rf {{}}".format(number))
+    # Get a list of all archives in the versions folder
+    archives = sorted(run('ls -1 /data/web_static/releases').split('\n'))
+
+    # Keep only the most recent `number` archives
+    if number >= len(archives):
+        return
+
+    archives_to_delete = archives[:-number]
+
+    # Delete archives in versions folder
+    for archive in archives_to_delete:
+        run('rm -f /data/web_static/releases/{}'.format(archive))
+
+    # Get a list of all archives in the local versions folder
+    local_archives = local('ls -1 versions', capture=True).split('\n')
+
+    # Keep only the most recent `number` local archives
+    if number >= len(local_archives):
+        return
+
+    local_archives_to_delete = local_archives[:-number]
+
+    # Delete local archives
+    for archive in local_archives_to_delete:
+        local('rm -f versions/{}'.format(archive))
