0f937167879290b1551fe1da6b7bab85437039c2
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-05 20:06:12 +0300

    finished

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,67 +1,102 @@
-# File: /etc/puppet/manifests/setup_web_static.pp
+# Puppet manifest to configure a web server for web_static deployment
 
-# Ensure Nginx is installed
+# Define Nginx configuration as a variable
+$nginx_conf = @(EOT)
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+}
+EOT
+
+# Install Nginx package
 package { 'nginx':
-  ensure => 'installed',
+  ensure   => 'present',
+  provider => 'apt',
 }
 
-# Create necessary directories
+# Create directory structure for web_static
 file { '/data':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure  => 'directory',
 }
 
 file { '/data/web_static':
   ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
 }
 
 file { '/data/web_static/releases':
   ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/data/web_static/shared':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
 }
 
 file { '/data/web_static/releases/test':
   ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
 }
 
-# Create the index.html file
+file { '/data/web_static/shared':
+  ensure => 'directory',
+}
+
+# Create index.html for test
 file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => '<html><head></head><body>Holberton School</body></html>',
+  ensure  => 'present',
+  content => "Holberton School Puppet\n",
 }
 
-# Create or recreate the symbolic link
+# Create a symbolic link to the current version
 file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test',
-  force   => true,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
 }
 
-# Update Nginx configuration
+# Set ownership of /data/ directory
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/',
+}
+
+# Create directory structure for Nginx
+file { '/var/www':
+  ensure => 'directory',
+}
+
+file { '/var/www/html':
+  ensure => 'directory',
+}
+
+# Create default index.html and 404.html pages
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n",
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n",
+}
+
+# Configure Nginx with the custom server block
 file { '/etc/nginx/sites-available/default':
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
+  ensure  => 'present',
+  content => $nginx_conf,
 }
 
-# Notify Nginx to restart when the configuration is updated
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+# Restart Nginx service
+exec { 'nginx restart':
+  path => '/etc/init.d/',
 }
