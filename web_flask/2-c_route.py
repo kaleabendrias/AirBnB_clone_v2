717c269f22eb23293ad77f0d0ede6c4904b9fb25
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-19 12:11:07 +0300

    finished

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""web application must be listening on 0.0.0.0"""
+from flask import Flask
+
+
+app = Flask('__name__')
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def Hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text=None):
+    sanitize = text.replace("_", " ")
+    return f"C {sanitize}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""web application must be listening on 0.0.0.0"""
+from flask import Flask
+
+
+app = Flask('__name__')
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def Hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text=None):
+    sanitize = text.replace("_", " ")
+    return f"C {sanitize}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def p_text(text="is cool"):
+    sanitize = text.replace("_", " ")
+    return f"Python {sanitize}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
