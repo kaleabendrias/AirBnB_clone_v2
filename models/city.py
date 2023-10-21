0642303ffd19556ca733bbfa06e0fc7020248c8b
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-21 19:23:44 +0300

    finished

--- models/city.py
+++ models/city.py
@@ -3,18 +3,22 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-    # Define the relationship with the Place class
-    places = relationship(
-            "Place",
-            backref="cities",
-            cascade="all, delete-orphan"
-            )
+        # Define the relationship with the Place class
+        places = relationship(
+                "Place",
+                backref="cities",
+                cascade="all, delete-orphan"
+                )
+    else:
+        name = ""
+        state_id = ""
--- models/user.py
+++ models/user.py
@@ -3,26 +3,33 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'  # Table name
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)  # Password column
-    first_name = Column(String(128))  # First name column (nullable)
-    last_name = Column(String(128))  # Last name column (nullable)
+    __tablename__ = 'users'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)  # Password column
+        first_name = Column(String(128))  # First name column (nullable)
+        last_name = Column(String(128))  # Last name column (nullable)
 
-    # Define the relationship with the Place class
-    places = relationship(
-            "Place",
-            backref="user",
-            cascade="all, delete-orphan"
-            )
+        # Define the relationship with the Place class
+        places = relationship(
+                "Place",
+                backref="user",
+                cascade="all, delete-orphan"
+                )
 
-    # Define the relationship with the Review class
-    reviews = relationship(
-            "Review",
-            backref="user",
-            cascade="all, delete-orphan"
-            )
+        # Define the relationship with the Review class
+        reviews = relationship(
+                "Review",
+                backref="user",
+                cascade="all, delete-orphan"
+                )
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""   
